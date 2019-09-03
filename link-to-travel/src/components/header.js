3a45b0fe1571cc846759c3e8d3a796ebd2aa0232
Tau Leong Lai <tl.lai@nettium.net>
2019-09-03 20:37:29 +0800

    commit bootstrap feature

--- link-to-travel/src/components/header.js
+++ link-to-travel/src/components/header.js
@@ -1,27 +1,49 @@
 import logo from '../logo.svg';
-import React from 'react';
+import React, { Component } from 'react';
 import '../App.css';
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+// import Row from 'react-bootstrap/Row'
+// import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
 
-function Header() {
-    return (
-        <header className="App-header">
-            <div className="App-LogoContainer">
+class Header extends Component {
+    constructor (){
+        super()
+        this.state={message : 'React-Bootstrap'}
+
+
+    }
+
+    changeMessage(){
+      alert(this.setState({
+        message:'sorry'
+    }));
+    }
+
+
+    render() {
+        return (
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <img src={logo} className="App-logo" alt="logo" />
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </div>
-            <div className="App-LoginContainer">
-                <div>
-                    <input type="text" className="txtboxSize" placeholder="Username" />
-                    <input type="text" className="txtboxSize" placeholder="Password"/>
-                    <input type="submit" className="" value="Login"/>
-                </div>
-                <div>
-                    <div className="text linkBtnForgetPwd">Forget Password ?</div>
-                </div>
-            </div>
-        </header>
-    );
+                <Navbar.Brand href="#home">{this.state.message}</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                    </Nav>
+                    <Nav>
+                        <Form inline>
+                            <FormControl type="text" placeholder="Username" className="mr-sm-2" />
+                            <FormControl type="password" placeholder="Password" className="mr-sm-2" />
+                            <Button variant="outline-success">Login</Button>
+                            <Button variant="outline-success" onClick={() => this.changeMessage()}>Sign Up</Button>
+                        </Form>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        );
+    }
 }
 export default Header
