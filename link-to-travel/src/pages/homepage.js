6a223a4bdb558b762cdc8e1233c0f1bbe98620ef
Tau Leong Lai <tl.lai@nettium.net>
2020-05-03 19:30:18 +0800

    commit Folder management

--- link-to-travel/src/pages/homepage.js
+++ link-to-travel/src/pages/homepage.js
@@ -3,16 +3,18 @@ import Header from '../components/Header/header';
 import Footer from '../components/Footer/footer';
 import Carousel from '../components/HomeCarousel';
 import Profile from '../components/User/profile';
+import NHeader from '../components/Header/nheader';
 
 class Homepage extends Component{
     render(){
         return(      
             <div className="HomeContanier">
-                <Header />
+                <NHeader/>
+
+                {/* <Header />
                 <Carousel />
-                <h1>Content here...</h1>
                 <Profile />
-                <Footer name="Page ended"> this is Fopter components.</Footer>
+                <Footer name="Page ended"> this is Fopter components.</Footer> */}
             </div>
         );
     }
